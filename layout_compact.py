a8906fedfc0a5923b2adf3eee86082adb365fce3
Ivan Simonini <drachlyznardh@roundhousecode.com>
2014-09-30 10:42:36 +0200

    Massive inclusion of members

--- layout_compact.py
+++ layout_compact.py
@@ -28,37 +28,40 @@ class Layout:
 		print "B {%s}" % transition
 
 	def draw_even_column(self, index, target):
-		return '│' # \u2502
+		self.layout += '│' # \u2502
 
 	def draw_odd_column(self, index, target):
-		return ' '
+		self.layout += ' '
 
 	def draw_layout (self, target):
 		
-		layout = ''
+		self.layout = ''
 
-		top_right = self.top.values()
-		top = None
-		top_left = []
-		bottom_right = self.bottom.values()
-		bottom = None
-		bottom_left = []
+		self.top_right = self.top.values()
+		self.top_current = None
+		self.top_left = []
+		self.bottom_right = self.bottom.values()
+		self.bottom_current = None
+		self.bottom_left = []
 		#print "T-- (%s) '%s' (%s)" % (top_left, top, top_right)
 		#print "B-- (%s) '%s' (%s)" % (bottom_left, bottom, bottom_right)
 
 		if self.size:
-			layout += self.draw_even_column(0, target)
+			self.draw_even_column(0, target)
 		for i in xrange(1, self.size):
-			layout += self.draw_odd_column(i, target)
-			layout += self.draw_even_column(i, target)
-		layout += '\n'
+			self.draw_odd_column(i, target)
+			self.draw_even_column(i, target)
+		self.layout += '\n'
 
 		for i in self.top:
 	
-			if top: top_left.append(top)
-			top = top_right.pop(0)
-			if bottom: bottom_left.append(bottom)
-			bottom = bottom_right.pop(0)
+			if self.top_current:
+				self.top_left.append(self.top_current)
+			self.top_current = self.top_right.pop(0)
+
+			if self.bottom_current:
+				self.bottom_left.append(self.bottom_current)
+			self.bottom_current = self.bottom_right.pop(0)
 			
 			#print "\tC[%d][%d] ^(%s) v(%s)" % (i, target.column, top, bottom)
 			#print "(%s) (%s)" % (hit_cols, missing_cols)
@@ -67,7 +70,7 @@ class Layout:
 
 			# Commit column, print the star
 			if i == target.column:
-				layout += '⬤' # \U2B24
+				self.layout += '⬤' # \U2B24
 				continue
 
 			# Every column before the commit has a preceeding whitespace
@@ -75,47 +78,47 @@ class Layout:
 			#	if target.hash in top_right: layout += '─' # \U2500
 			#	else: layout += ' '
 
-			if top != '':
+			if self.top_current != '':
 				
-				if bottom != '':
+				if self.bottom_current != '':
 
 					if i > target.column: # after the target commit
-						layout += ' '
+						self.layout += ' '
 	
-					if top == bottom:
-						layout += '│'
-					elif bottom == target.hash:
-						layout += '├'
+					if self.top_current == self.bottom_current:
+						self.layout += '│'
+					elif self.bottom_current == target.hash:
+						self.layout += '├'
 					else:
-						layout += '?'
+						self.layout += '?'
 
 					if i < target.column: # before the target commit
 						# Both top and bottom cells have values
-						if (target.hash == bottom or target.hash in bottom_left) and (target.hash == top or target.hash in top_right):
-							layout += '→' # \U251C \u2500
-						elif top == bottom:
+						if (target.hash == self.bottom_current or target.hash in self.bottom_left) and (target.hash == self.top_current or target.hash in self.top_right):
+							self.layout += '→' # \U251C \u2500
+						elif self.top_current == self.bottom_current:
 							#if target.hash == top_right[0]:
 							#	layout += '│→' # \U2502
 							#else:
-							layout += ' ' # \U2502
-						elif target.hash == bottom and target.hash in bottom_right:
-							layout += '→'
+							self.layout += ' ' # \U2502
+						elif target.hash == bottom_current and target.hash in bottom_right:
+							self.layout += '→'
 						else:
-							layout += '?'
+							self.layout += '?'
 
 				else:
-					if top == target.hash:
+					if self.top_current == target.hash:
 						#or target.hash in top_right:
-						layout += '→┘' # \u2500 \U2518
-					else: layout += '^'
+						self.layout += '→┘' # \u2500 \U2518
+					else: self.layout += '^'
 
-			elif bottom != '':
+			elif self.bottom_current != '':
 				
-				if bottom == target.hash:
-					layout += '←┐' # \u2500 \U2510
-				else: layout += 'v'
+				if self.bottom_current == target.hash:
+					self.layout += '←┐' # \u2500 \U2510
+				else: self.layout += 'v'
 
-			else: layout += '  '
+			else: self.layout += '  '
 
 			# Every column after the commit has a following whitespace
 			#if i < target.column:
@@ -123,5 +126,5 @@ class Layout:
 			#		layout += '─' # \U2500
 			#	else: layout += ' '
 
-		return layout
+		return self.layout
 
