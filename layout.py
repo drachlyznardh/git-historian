82a0a060243277ea6141f5636c23a9b11255be09
Ivan Simonini <drachlyznardh@roundhousecode.com>
2014-10-06 09:20:07 +0200

    Invoking two chars

--- layout.py
+++ layout.py
@@ -18,7 +18,7 @@ class Layout:
 		self.bottom = {}
 		for i in xrange(size):
 			self.bottom[i] = ''
-		self.last = ''
+		#self.last = ''
 
 		self.layout = []
 
@@ -41,7 +41,7 @@ class Layout:
 			else: transition += " %s" % "XXXXXXX"
 		print "B {%s}" % transition
 
-	def put_char(self, name, symbol):
+	def put_char(self, name, transition, padding):
 		
 		if name == None:
 			color = 39
@@ -56,14 +56,14 @@ class Layout:
 			if name / 6 % 2: style = 1
 			else: style = 22
 
-		self.layout.append(Column(color, style, symbol, symbol))
-		self.last = symbol
+		self.layout.append(Column(color, style, transition, padding))
+		#self.last = symbol
 		#self.layout += '\x1b[3%dm%s' % (color, symbol)
 
 	def draw_even_column(self, index, target):
 		
 		if index == target.column:
-			self.put_char(None, '•') # \u2022
+			self.put_char(None, '•', '│') # \u2022 \u2502
 			return
 
 		top = self.top[index]
@@ -77,43 +77,48 @@ class Layout:
 
 					if target.hash in self.ne:
 						
-						self.put_char(bottom, '├') # \u251c
+						self.put_char(bottom, '├', '│') # \u251c
 					elif target.hash in self.nw:
 						
-						self.put_char(top, '┤') # \u2524
+						self.put_char(top, '┤', '│') # \u2524
 					elif target.hash in self.commit[bottom].child:
 
-						self.put_char(bottom, '├') # \u251c
-					else: self.layout += '^'
+						self.put_char(bottom, '├', '│') # \u251c
+					else: #self.layout += '^'
+						self.put_char(None, '^', '^')
 
 				else: 
-					self.put_char(top, '│') # \u2502
+					self.put_char(top, '│', '│') # \u2502
 
-			else: self.layout += '@'
+			else: #self.layout += '@'
+				self.put_char(None, '@', '@')
 
 			return
 
 		if len(bottom): # only lower end is present
 
 			if bottom == target.hash:
-				self.layout += '┐' # \u2510
+				#self.layout += '┐' # \u2510
+				self.put_char(None, '┐', '│') # \u2510 \u2502
 				return
 
 			if index > target.column:
-				self.put_char(index, '┐') # \u2510
+				self.put_char(index, '┐', '│') # \u2510 \u2502
 				return
 			else:
-				self.put_char(index, '┌') # \u250c
+				self.put_char(index, '┌', '│') # \u250c \u2502
 				return
 				
 		if len(top): # only upper end is present
 
-			self.put_char(None, '_')
+			self.put_char(None, '_', '_')
 			return
 
-		if self.last == '←' or self.last == '→':
-			self.layout += '─' # \u2500
-		else: self.layout += ' '
+		last = self.layout[-1]
+		if last.transition == '←' or last.transition == '→':
+			transition = '─' # \u2500
+		else: transition = ' '
+		self.put_char(None, transition, ' ')
 
 	def draw_odd_column(self, index, target):
 
@@ -123,17 +128,17 @@ class Layout:
 			
 			for name in target.parent:
 				if name in self.se:
-					self.put_char(name, '←')
+					self.put_char(name, '←', ' ')
 					return
 		
 		else:
 
 			for name in reversed(target.parent):
 				if name in self.sw:
-					self.put_char(name, '→')
+					self.put_char(name, '→', ' ')
 					return
 
-		self.put_char(None, ' ')
+		self.put_char(None, ' ', ' ')
 
 	def draw_layout (self, target, padding = 1):
 		
@@ -148,12 +153,16 @@ class Layout:
 		#print "South %s" % self.se
 
 		if padding:
-			if self.ne[0]: self.layout += '│' # \u2502
-			else: self.layout += ' '
+			if self.ne[0]: #self.layout += '│' # \u2502
+				self.put_char(None, '│', '│') # \u2502
+			else: #self.layout += ' '
+				self.put_char(None, ' ', ' ')
 			for i in self.ne[1:]:
-				if i: self.layout += ' │'
-				else: self.layout += '  '
-			self.layout += '\n'
+				if i: #self.layout += ' │'
+					self.put_char(None, '│', '│') # \u2502
+				else: #self.layout += '  '
+					self.put_char(None, ' ', ' ')
+			#self.layout += '\n'
 
 		if self.size:
 			self.draw_even_column(0, target)
