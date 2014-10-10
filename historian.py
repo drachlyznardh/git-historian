465a5766f9413f4752e8fc92fc3072f2606ccd44
Ivan Simonini <drachlyznardh@roundhousecode.com>
2014-10-10 15:31:01 +0200

    Cleaning

--- historian.py
+++ historian.py
@@ -90,7 +90,6 @@ class Historian:
 		if debug:
 			print '\n-- Vertical unrolling --'
 
-		#visit = vertical.Order()
 		visit = horizontal.Order(0)
 
 		for name, commit in self.commit.items():
@@ -102,7 +101,6 @@ class Historian:
 				continue
 
 			if debug: print 'pushing %s' % name[:7]
-			#visit.push(name)
 			visit.push_one(name)
 
 			while 1:
@@ -133,17 +131,9 @@ class Historian:
 					print '  Now pushing %d children' % children
 					visit.push_many(commit.child)
 					continue
-					#skip = 0
-					#for i in reversed(commit.child):
-					#	child = self.commit[i]
-					#	if child and not child.done:
-					#		visit.cpush(i)
-					#		skip = 1
-					#if skip: continue
 				elif children > 0:
 					child = self.commit[commit.child[0]]
 					if child and not child.done:
-						#visit.cpush(commit.child[0])
 						print '  Now pushing single child'
 						visit.push_one(commit.child[0])
 						continue
@@ -159,16 +149,11 @@ class Historian:
 				parents = len(commit.parent)
 
 				if parents > 1:
-					#for i in commit.parent:
-					#	parent = self.commit[i]
-					#	if parent and not parent.done:
-					#		visit.ppush(i)
 					print '  Now pushing %d parents' % parents
 					visit.push_many(commit.parent)
 				elif parents > 0:
 					parent = self.commit[commit.parent[0]]
 					if parent and not parent.done:
-						#visit.push(commit.parent[0])
 						print '  Now pushing single parent'
 						visit.push_one(commit.parent[0])
 				
@@ -181,7 +166,6 @@ class Historian:
 
 	def print_graph (self, debug):
 		
-		#t = layout.Layout(self.width, self.commit, debug)
 		t = layout.Layout(self.width, self.commit, debug)
 
 		cmdargs = 'git show -s --oneline --decorate --color'.split(' ')
