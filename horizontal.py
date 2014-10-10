83a17676d80b6a147c9942be83c101b58dbffa8c
Ivan Simonini <drachlyznardh@roundhousecode.com>
2014-10-10 11:19:09 +0200

    New class

--- horizontal.py
+++ horizontal.py
@@ -1,52 +1,16 @@
 # Column module for Git-Historian
 
-class Column:
+class Cell:
 
-	def __init__ (self, l, count):
-		self.content = l
-		self.index = -1
-		self.available = 0
-		self.count = count
+	def __init__ (self, name):
+		self.name = name
+		self.left = self.top = self.bottom = ''
+		self.upper = self.lower = ''
 	
-	def make_available (self):
-		self.content = []
-		self.index = -1
-		self.available = 1
-		self.count = 0
-
-	def top (self):
-		if len(self.content) == 0: return ''
-		return self.content[0]
-	
-	def bottom (self):
-		if len(self.content) == 0: return ''
-		return self.content[-1]
-
-	def last2bottom (self):
-		if len(self.content) < 2: return ''
-		return self.content[-2]
-
-	def append (self, commit):
-		self.available = 0
-		self.content.append(commit.hash)
-		self.count = len(commit.parent)
-
-	def show (self):
-		if len(self.content) == 0:
-			print '%d [Avail]' % self.count
-			return
-
-		line = '%d [%s' % (self.count, self.content[0][:7])
-		for i in self.content[1:]:
-			line += ', ' + i[:7]
-		line += ']'
-		print "%s" % line
-
 class Order:
 
 	def __init__ (self, debug):
 		self.content = []
-		#self.commit = commit
 		self.debug = debug
 
 	def show (self):
