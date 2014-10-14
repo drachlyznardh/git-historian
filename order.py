0b4ab67e85e103c315b34b240d081525cb822246
Ivan Simonini <drachlyznardh@roundhousecode.com>
2014-10-14 09:22:27 +0200

    This order is simple

--- order.py
+++ order.py
@@ -4,4 +4,18 @@ class LeftmostFirst:
 
 	def __init__ (self):
 
-		self.content = [[]]
+		self.content = []
+
+	def push (self, l):
+
+		for e in reversed(l):
+			self.content.insert(0, e)
+
+	def is_empty (self):
+
+		return len(self.content) == 0
+
+	def pop (self):
+
+		try: return self.content.pop(0)
+		except: return None
