1a5f77cc77ad6b6db60e88404e0ecd29d0f28704
Ivan Simonini <drachlyznardh@roundhousecode.com>
2014-10-21 14:06:22 +0200

    Loading names from cmdline or file

--- headhunter.py
+++ headhunter.py
@@ -4,6 +4,7 @@ from subprocess import check_output
 from subprocess import CalledProcessError
 import re
 import sys
+import json
 
 class HeadHunter:
 
@@ -12,6 +13,29 @@ class HeadHunter:
 		self.head = []
 		self.debug = debug
 
+		self.name = []
+		self.cname= []
+
 	def hunt (self, args):
 
+		self.load_configfile('.git-historian')
+		self.load_args(args)
+
+		print '(%s)' % ', '.join(self.name)
+
 		return self.head
+
+	def load_configfile (self, target_file):
+
+		try: f = open(target_file, 'r')
+		except IOError as e: return
+
+		self.cname = json.load(f)
+
+	def load_args (self, args):
+
+		if len(args) == 0:
+			self.name.extend(self.cname)
+			return
+
+		self.name.extend(args)
