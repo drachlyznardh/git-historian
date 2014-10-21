25420dd4c59f45c29f8b1e8175475050242372dc
Ivan Simonini <drachlyznardh@roundhousecode.com>
2014-10-21 13:33:46 +0200

    Here comes a new class

---
+++ headhunter.py
@@ -0,0 +1,16 @@
+# Headhunter module for Git-Historian
+
+from subprocess import check_output
+from subprocess import CalledProcessError
+import re
+import sys
+
+class HeadHunter:
+
+	def __init__ (self):
+
+		self.head = []
+
+	def hunt (self, args):
+
+		return self.head
--- historian.py
+++ historian.py
@@ -7,6 +7,7 @@ import re
 import sys
 import getopt
 
+import headhunter
 import node
 import order
 
