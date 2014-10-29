e1ce6a7062b9a89a463d12df0d5cfe19520d60a6
Ivan Simonini <drachlyznardh@roundhousecode.com>
2014-10-29 10:55:03 +0100

    Factoring out the Option class

---
+++ option.py
@@ -0,0 +1,66 @@
+# Option module for Git-Historian
+# -*- encoding: utf-8 -*-
+
+import sys
+import getopt
+
+class Option:
+
+	def __init__ (self):
+
+		self.verbose = 0
+		self.debug = 0
+		self.all_debug = 0
+
+		self.all_heads = 0
+		self.head = []
+
+	def print_version(self):
+		print "Git-Historian %s (C) 2014 Ivan Simonini" % VERSION
+
+	def print_help(self):
+		print "Usage: %s [options] heads…" % sys.argv[0]
+		print
+		print ' -D, --all-debug : print all kinds of debug messages'
+		print ' -d N, --debug N : add N to the debug counter'
+		print
+		print 'debug  1 : show heads'
+		print 'debug  2 : show data loading'
+		print 'debug  4 : show bindings'
+		print 'debug  8 : show vertical unroll'
+		print 'debug 16 : show column assignments'
+		print 'debug 32 : show layout construction'
+
+	def parse (self):
+
+		try:
+			optlist, args = getopt.gnu_getopt(sys.argv[1:], 'ahvDd:',
+				['help', 'verbose', 'version',
+				'all', 'all-heads',
+				'debug', 'all-debug'])
+		except getopt.GetoptError as err:
+			print str(err)
+			self.print_help()
+			sys.exit(2)
+
+		for key, value in optlist:
+			if key in ('-h', '--help'):
+				self.print_help()
+				sys.exit(0)
+			elif key in ('-v', '--verbose'):
+				self.verbose = 1
+			elif key in ('-a', '--all', '--all-heads'):
+				self.all_heads = 1
+			elif key in ('-D', '--all-debug'):
+				self.all_debug = 1
+			elif key in ('-d', '--debug'):
+				self.debug += int(value)
+			elif key == '--version':
+				self.print_version()
+				sys.exit(0)
+
+		self.args = args
+	
+	def d (self, value):
+		return self.all_debug or self.debug / value % 2
+
