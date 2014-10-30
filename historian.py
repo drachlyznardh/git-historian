a43bd370acc103982bfb470251093f92ea114262
Ivan Simonini <drachlyznardh@roundhousecode.com>
2014-10-30 16:41:03 +0100

    Shorter

--- historian.py
+++ historian.py
@@ -300,13 +300,9 @@ class Historian:
 			
 			t.compute_layout(node)
 
-			#message = h.describe(name)
-			#message = [node.to_oneline()]
-			message = node.message
-
 			try:
-				print '\x1b[m%s\x1b[m %s' % (t.draw_transition(), message[0])
-				for i in message[1:-1]:
+				print '\x1b[m%s\x1b[m %s' % (t.draw_transition(), node.message[0])
+				for i in node.message[1:-1]:
 					print '\x1b[m%s\x1b[m %s' % (t.draw_padding(), i)
 			except IOError as error: return
 
