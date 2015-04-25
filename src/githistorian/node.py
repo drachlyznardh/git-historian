55e7505a031a3d5f7ee01e4e14ec1a482e138d9a
Ivan Simonini <drachlyznardh@roundhousecode.com>
2015-04-25 11:32:48 +0200

    Fixes

--- src/githistorian/node.py
+++ src/githistorian/node.py
@@ -10,19 +10,18 @@ class Node:
 		self.child = []
 
 		self.message = None
+		self.done = 0
 
 		self.column = -1
 		self.border = -1
 		self.row = -1
 
-		self.done = 0
 		self.top = None    # Previous commit by line
 		self.bottom = None # Next commit by line
 		self.left = None   # Previous commit by column
 		self.right = None  # Next commit by line
 
 	def add_child (self, name):
-
 		if name not in self.child:
 			self.child.append(name)
 
@@ -37,21 +36,14 @@ class Node:
 		self.border = max(self.border, value)
 
 	def get_indent (self):
-		if self.column > 0:
-			return ('%%%ds' % (3 * self.column)) % (' ')
-		return ''
+		return ' ' * 3 * self.column
 
 	def to_oneline(self):
-	
-		return '(%2d, %2d)%s • \x1b[33m%s\x1b[m' % (
-			self.column, self.row,
-			self.get_indent(),
-			self.name[:7])
+		data = (self.column, self.row, self.get_indent(), self.name[:7])
+		return '(%2d, %2d)%s • \x1b[33m%s\x1b[m' % data
 
 	def to_string(self):
-		if self.column > 0:
-			indent = ('%%%ds' % (2 * self.column)) % (' ')
-		else: indent = ''
+		indent = ' ' * 2 * self.column
 		str = "%s  Name {%s}" % (indent, self.name)
 		for i in self.parent: str += "\n%sParent {%s}" % (indent, i)
 		for i in self.child:  str += "\n%s Child {%s}" % (indent, i)
