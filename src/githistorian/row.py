54dbeea7b663370fe32330ae1d42c1f1c59d540e
Ivan Simonini <drachlyznardh@roundhousecode.com>
2015-04-25 21:39:47 +0200

    Split

--- src/githistorian/row.py
+++ src/githistorian/row.py
@@ -9,6 +9,57 @@ class Row:
 		self.db = db
 		self.heads = heads
 
+	def if_done (self, name, target):
+
+		# No need to drop down beyond the last element
+		if self.previous == target.name: return
+
+		# Binding top and bottom nodes together
+		if target.top:
+			self.db.at(target.top).bottom = target.bottom
+		self.db.at(target.bottom).top = target.top
+
+		# Binding previous and current nodes together
+		target.top = self.previous
+		self.db.at(previous).bottom = name
+
+		# Bumping the row number another time
+		row += 1
+		target.row = row
+
+		# This node is now the last
+		target.bottom = None
+
+		# Recording current node as the next previous
+		self.previous = name
+
+	def if_not_done (self, name, target):
+
+		# No node can appear before any of its children
+		children = self.db.skip_if_done(target.child)
+		if len(children): return
+
+		# Bind this node with the previous, if any, or…
+		if self.previous:
+			target.top = self.previous
+			self.db.at(self.previous).bottom = name
+
+		# … record this node as the first in the chain
+		else: self.first = name
+
+		# Bumping the row number
+		self.row += 1
+		target.row = self.row
+
+		# Add parents to the visit
+		self.visit.push(self.db.skip_if_done(target.parent))
+
+		# The current node is the next previous
+		self.previous = name
+
+		# The current node is done
+		target.done = 1
+
 	def unroll (self, debug):
 
 		if debug: print '-- Row Unroll --'
@@ -36,55 +87,8 @@ class Row:
 
 			# Even if done, a node can drop down in the chain after its
 			# last-calling child
-			if target.done:
-
-				# No need to drop down beyond the last element
-				if self.previous == target.name: continue
-
-				# Binding top and bottom nodes together
-				if target.top:
-					self.db.at(target.top).bottom = target.bottom
-				self.db.at(target.bottom).top = target.top
-
-				# Binding previous and current nodes together
-				target.top = self.previous
-				self.db.at(previous).bottom = name
-
-				# Bumping the row number another time
-				row += 1
-				target.row = row
-
-				# This node is now the last
-				target.bottom = None
-
-				# Recording current node as the next previous
-				self.previous = name
-				continue
-
-			# No node can appear before any of its children
-			children = self.db.skip_if_done(target.child)
-			if len(children): continue
-
-			# Bind this node with the previous, if any, or…
-			if self.previous:
-				target.top = self.previous
-				self.db.at(self.previous).bottom = name
-
-			# … record this node as the first in the chain
-			else: self.first = name
-
-			# Bumping the row number
-			self.row += 1
-			target.row = self.row
-
-			# Add parents to the visit
-			self.visit.push(self.db.skip_if_done(target.parent))
-
-			# The current node is the next previous
-			self.previous = name
-
-			# The current node is done
-			target.done = 1
+			if target.done: self.if_done(name, target)
+			else: self.if_not_done(name, target)
 
 		return self.first
 
