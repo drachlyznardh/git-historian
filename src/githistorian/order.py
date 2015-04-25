7254020f7d27bc5980ea2f0c8878b49d4cc37ccf
Ivan Simonini <drachlyznardh@roundhousecode.com>
2015-04-25 21:47:16 +0200

    Removal

--- src/githistorian/order.py
+++ src/githistorian/order.py
@@ -1,34 +1,5 @@
 # Order module for Git-Historian
 
-class ColumnOrder:
-
-	def __init__ (self):
-		self.content = []
-
-	def has_more (self):
-		return len(self.content)
-
-	def push (self, arg):
-
-		if isinstance(arg, basestring):
-			self.content.append(arg)
-			return
-
-		if not isinstance(arg, list):
-			print 'WTF is %s?' % arg
-			return
-
-		if len(arg) == 0: return
-
-		self.content.extend(arg)
-
-	def pop (self):
-		try: return self.content.pop(0)
-		except: return None
-
-	def show (self):
-		return '    [%s]' % ', '.join([e[:7] for e in self.content])
-
 class LeftmostFirst:
 
 	def __init__ (self):
