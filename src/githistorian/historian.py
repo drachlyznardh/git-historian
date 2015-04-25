33ea3c324729c04759f3985b7d24bc6ce9202a53
Ivan Simonini <drachlyznardh@roundhousecode.com>
2015-04-25 20:54:18 +0200

    More

--- src/githistorian/historian.py
+++ src/githistorian/historian.py
@@ -76,6 +76,85 @@ def _bind_children (debug, heads, db):
 
 		commit.done = 1
 
+def _row_unroll (debug, heads, db):
+
+	if debug: print '-- Row Unroll --'
+
+	# Visit starts with all the heads
+	visit = RowOrder()
+	visit.push(heads)
+
+	# Reference to previous node, to build the chain
+	previous = None
+
+	# Starting over the first row
+	row = -1
+
+	# The first node
+	first = None
+
+	while visit.has_more():
+
+		name = visit.pop()
+		target = db.at(name)
+
+		if debug:
+			print 'Visiting %s %s' % (name[:7], visit.show())
+
+		# Even if done, a node can drop down in the chain after its
+		# last-calling child
+		if target.done:
+
+			# No need to drop down beyond the last element
+			if previous == target.name: continue
+
+			# Binding top and bottom nodes together
+			if target.top:
+				db.at(target.top).bottom = target.bottom
+			db.at(target.bottom).top = target.top
+
+			# Binding previous and current nodes together
+			target.top = previous
+			db.at(previous).bottom = name
+
+			# Bumping the row number another time
+			row += 1
+			target.row = row
+
+			# This node is now the last
+			target.bottom = None
+
+			# Recording current node as the next previous
+			previous = name
+			continue
+
+		# No node can appear before any of its children
+		children = db.skip_if_done(target.child)
+		if len(children): continue
+
+		# Bind this node with the previous, if any, or…
+		if previous:
+			target.top = previous
+			db.at(previous).bottom = name
+
+		# … record this node as the first in the chain
+		else: first = name
+
+		# Bumping the row number
+		row += 1
+		target.row = row
+
+		# Add parents to the visit
+		visit.push(self.db.skip_if_done(target.parent))
+
+		# The current node is the next previous
+		previous = name
+
+		# The current node is done
+		target.done = 1
+
+	return first
+
 class Historian:
 
 	def __init__ (self, opt):
@@ -89,80 +168,6 @@ class Historian:
 
 	def update_width (self, value):
 		self.width = max(self.width, value)
-
-	def row_unroll (self, debug):
-
-		if debug: print '-- Row Unroll --'
-
-		# Visit starts with all the heads
-		visit = RowOrder()
-		visit.push(self.head)
-
-		# Reference to previous node, to build the chain
-		previous = None
-
-		# Starting over the first row
-		row = -1
-
-		while visit.has_more():
-
-			name = visit.pop()
-			target = self.db.at(name)
-
-			if debug:
-				print 'Visiting %s %s' % (name[:7], visit.show())
-
-			# Even if done, a node can drop down in the chain after its
-			# last-calling child
-			if target.done:
-
-				# No need to drop down beyond the last element
-				if previous == target.name: continue
-
-				# Binding top and bottom nodes together
-				if target.top:
-					self.db.at(target.top).bottom = target.bottom
-				self.db.at(target.bottom).top = target.top
-
-				# Binding previous and current nodes together
-				target.top = previous
-				self.db.at(previous).bottom = name
-
-				# Bumping the row number another time
-				row += 1
-				target.row = row
-
-				# This node is now the last
-				target.bottom = None
-
-				# Recording current node as the next previous
-				previous = name
-				continue
-
-			# No node can appear before any of its children
-			children = self.db.skip_if_done(target.child)
-			if len(children): continue
-
-			# Bind this node with the previous, if any, or…
-			if previous:
-				target.top = previous
-				self.db.at(previous).bottom = name
-
-			# … record this node as the first in the chain
-			else: self.first = name
-
-			# Bumping the row number
-			row += 1
-			target.row = row
-
-			# Add parents to the visit
-			visit.push(self.db.skip_if_done(target.parent))
-
-			# The current node is the next previous
-			previous = name
-
-			# The current node is done
-			target.done = 1
 
 	def find_column_for_head (self, name, debug):
 
@@ -339,7 +344,8 @@ def tell_the_story():
 	# Graph unrolling
 	_bind_children(opt.d(4), heads, db)
 	db.clear()
-	self.row_unroll(self.o.d(8))
+	first = _row_unroll(opt.d(8), heads, db)
+	#self.row_unroll(self.o.d(8))
 	db.clear()
 	self.column_unroll(self.o.d(16))
 	self.print_graph(self.o.d(32))
