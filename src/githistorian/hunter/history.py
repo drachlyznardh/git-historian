721aac88009f386620f0ee0df8125d16c2fd6dad
Ivan Simonini <drachlyznardh@roundhousecode.com>
2015-04-26 11:10:52 +0200

    Wasteing less vertical space

--- src/githistorian/hunter/history.py
+++ src/githistorian/hunter/history.py
@@ -12,21 +12,16 @@ def _select_pretty (value):
 	if value: return r'--pretty=%H %P#' + value
 	return r'--pretty=%H %P#%C(yellow)%h%C(auto)%d%Creset %s %C(bold red)%ar%Cblue %an'
 
+# Invokes git-log with optional size limit to collect commits, their relation
+# with others and the custom messages
 def _get_history_dump (opt, debug, heads, limit):
 
-	# Looking for commit's and parents' names…
 	cmdlist = ['git', 'log', _select_pretty(opt.pretty)]
-
-	# Optional limit to the size of the history
 	if limit: cmdlist.append('-n%d' % limit)
-
-	# … starting from know heads only
 	cmdlist.extend(heads)
 
-	# Print the request
 	if debug: print(cmdlist)
 
-	# Invoking Git
 	return check_output(cmdlist)
 
 def hunt (opt, debug, heads, limit):
