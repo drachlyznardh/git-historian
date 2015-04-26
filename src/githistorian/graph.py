a9fef3be3d458fdb46e98885c00a3ac8f640298c
Ivan Simonini <drachlyznardh@roundhousecode.com>
2015-04-26 14:25:38 +0200

    Using less vertical space

--- src/githistorian/graph.py
+++ src/githistorian/graph.py
@@ -7,26 +7,20 @@ from .layout import Layout
 class VisitOrder:
 
 	def __init__ (self):
-
 		self.content = []
 
 	def push (self, arg):
-
 		if len(arg) == 0: return
-
 		for e in reversed(arg):
 			self.content.insert(0, e)
 
 	def is_empty (self):
-
 		return len(self.content) == 0
 
 	def has_more (self):
-
 		return len(self.content)
 
 	def pop (self):
-
 		try: return self.content.pop(0)
 		except: return None
 
