dd1cb122727125fcc682c0f05ad9141431ccfa3a
Ivan Simonini <gods@roundhousecode.com>
2019-05-14 19:07:27 +0200

    Visiting...

--- src/githistorian/alternate.py
+++ src/githistorian/alternate.py
@@ -1,4 +1,29 @@
 
+class Order:
+	def __init__ (self):
+		self.order = []
+
+	def __bool__ (self):
+		return True if self.order else False
+
+	def push (self, arg):
+		if arg: self.order = arg + self.order
+
+	def pop (self):
+		try: return self.order.pop(0)
+		except: return None
+
+def reduce_graphs(opt, heads, db):
+
+	grid = []
+	order = Order()
+
+	order.push(heads)
+
+	while order:
+		name = order.pop()
+		print('Visiting {}'.format(name))
+
 def deploy(opt, heads, db):
 
 	try:
@@ -6,6 +31,8 @@ def deploy(opt, heads, db):
 		print(heads)
 		print(db)
 	except BrokenPipeError: pass
+
+	reduce_graphs(opt, heads, db)
 
 	return 0
 
