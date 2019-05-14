3d9668759aeaa9c046036bcdc4638edcbd37d82e
Ivan Simonini <gods@roundhousecode.com>
2019-05-14 18:38:13 +0200

    Fixing

--- setup.py
+++ setup.py
@@ -1,43 +1,11 @@
 
 from setuptools import setup, find_packages
-from setuptools.extension import Extension
-from distutils.command.build_py import build_py
-from distutils.command.install_lib import install_lib
 
 with open('VERSION', 'r') as ifd:
     version = ifd.read().strip()
 
-class MyBuildPy(build_py):
-
-	def addCustomVersion(self):
-		import os
-		from distutils.file_util import copy_file
-		_, srcdir, destdir, _ = self.data_files[0]
-		copy_file('VERSION', os.path.join(destdir, 'VERSION'))
-		# copy_file(os.path.join(srcdir, 'VERSION'), os.path.join(destdir, 'VERSION'))
-
-	def run(self):
-		build_py.run(self)
-		if not self.dry_run: self.addCustomVersion()
-
-class MyInstallLib(install_lib):
-
-	def addCustomVersion(self):
-		print(self)
-		for e in self.__dict__: print(e)
-		print(self.distribution)
-		print('From {} to {}'.format(self.build_dir, self.install_dir))
-		return
-		import os
-		from distutils.file_util import copy_file
-		_, srcdir, destdir, _ = self.data_files[0]
-		# copy_file('VERSION', os.path.join(destdir, 'VERSION'))
-		# copy_file(os.path.join(srcdir, 'VERSION'), os.path.join(destdir, 'VERSION'))
-		print(self.data_files)
-
-	def run(self):
-		install_lib.run(self)
-		if not self.dry_run: self.addCustomVersion()
+with open('README.md', 'r') as ifd:
+	readme = ifd.read()
 
 setup(
 	name='githistorian',
@@ -45,9 +13,17 @@ setup(
 	url='https://github.com/drachlyznardh/githistorian',
 	author='Ivan Simonini',
 	author_email='drachlyznardh@gmail.com',
+	description='Graph visualizer for git history',
+	long_description=readme,
+	long_description_content_type='text/markdown',
 	package_dir={'':'src'},
 	packages=find_packages(where='src'),
 	install_requires=['bintrees'],
 	entry_points = { 'console_scripts': ['githistorian=githistorian.githistorian:tell_the_story'] },
-	cmdclass = { 'build_py': MyBuildPy, 'install_lib': MyInstallLib }
+	package_data={'': ['VERSION']},
+	classifiers=[
+		"Programming Language :: Python :: 3",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+	],
 )
