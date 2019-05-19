af511d11da39bcd31277b90e900193cb89fe766a
Ivan Simonini <gods@roundhousecode.com>
2019-05-19 17:30:52 +0200

    Adding plenty of structure and indirection

--- src/githistorian/alternate.py
+++ src/githistorian/alternate.py
@@ -178,20 +178,62 @@ class Grid:
 
 from enum import Enum
 
+# Single-value representation of flip
+class FlipState(Enum):
+	NONE  = 0 # No flip, top to bottom, left to right
+	HFLIP = 1 # Horizontal flip, top to bottom, right to left
+	VFLIP = 2 # Vertical flip, bottom to top, left to right
+	BOTH  = 3 # Both flips, bottom to top, right to left
+
 # Helper class for even, unrepeatable columns holding commits and relationships
-class EvenColumnState(Enum):
+class EvenColumn(Enum):
 	EMPTY   = 0 # Column is empty with no arrow
 	SOURCE  = 1 # Column holds the current commit
 	LCORNER = 2 # Left corner, arrow bending up from yet unseen source node
 	RCORNER = 3 # Right corner, arrow bending up from already seen source node
 	LMERGE  = 4 # Left merge, arrow joining from yet unseen source node
 	RMERGE  = 5 # Right merge, arrow joining from already seen source node
+	PIPE    = 6 # Straight line
+	LARROW  = 7 # Arrow towards target's column from yet unseen source node
+	RARROW  = 8 # Arrow towards target's column from already seen source node
+
+	def get(self, flip, debug):
+		if debug: return '{}'.format(self.value)
+		return {
+				EvenColumn.EMPTY   : '    ',
+				EvenColumn.SOURCE  : ['\x1b[m{}' for e in range(4)],
+				EvenColumn.LCORNER : '└┘┌┐', # U+2514 2518 250c 2510
+				EvenColumn.RCORNER : '┘└┐┌', # U+2518 2514 2510 250c
+				EvenColumn.LMERGE  : '┤├┤├', # U+251c 2524 251c 2524
+				EvenColumn.RMERGE  : '├┤├┤', # U+251c 2524 251c 2524
+				EvenColumn.PIPE    : '││││', # U+2502 2502 2502 2502
+				EvenColumn.LARROW  : '←→←→',
+				EvenColumn.RARROW  : '→←→←',
+			}[self][flip.value]
 
 # Helper class for odd, repeatble columns holding only arrows
-class OddColumnState(Enum):
+class OddColumn(Enum):
 	EMPTY   = 0 # Column is empty
 	LARROW  = 1 # Arrow towards target's column from yet unseen source node
 	RARROW  = 2 # Arrow towards target's column from already seen source node
+
+	def _get(self, flip, debug):
+		if debug: return '{}'.format(self.value)
+		return {
+				OddColumn.EMPTY  : '    ',
+				OddColumn.LARROW : '←→←→',
+				OddColumn.RARROW : '→←→←',
+			}[self][flip.value]
+
+	def get(self, flip, debug, counter):
+		symbol = self._get(flip, debug)
+		if counter == 1: return symbol
+		return ''.join([symbol for e in range(counter)])
+
+class RowState(Enum):
+	CHAINFIRST  = 0 # First line of first commit in chain
+	COMMITFIRST = 1 # First line of commit, second in chain or later
+	CONTENT     = 2 # Second or following line in commit
 
 # This grid is simple and dumb, it assigns a new column to each chain
 class DumbGrid:
@@ -205,17 +247,44 @@ class DumbGrid:
 		def parentSeen(self, node):
 			self.parents.remove(node.topName)
 
+	class Row:
+		def __init__(self, nodeName, columns):
+			self.nodeName = nodeName
+			self.columns = columns
+
+		# Append empty columns to match the target size
+		def extend(self, targetSize):
+			l = len(self.columns)
+			# print('\tExtending from {} to {} columns'.format(l, targetSize))
+			if l == targetSize: return # We already match the layout size
+			# print('\tExtending to {} columns, which is different from {}'.format(targetSize, l))
+			self.columns.extend([('', EvenColumn.EMPTY, OddColumn.EMPTY) for e in range(targetSize - l)])
+
+		def dump(self, db, oddRange):
+			e = db[self.nodeName]
+			flip = FlipState.NONE
+			debug = False
+			# # # return ''.join(['{}{}'.format(e.get(flip, debug), o.get(flip, debug, oddRange)) for e,o in self.columns]) #.format(e.getContent())
+			# # return ''.join([e.get(flip, debug) + o.get(flip, debug, oddRange) for e,o in self.columns]) + '{}'.format(e.getContent()[0])
+			# line = ''.join([c + e.get(flip, debug) + o.get(flip, debug, oddRange) for c,e,o in self.columns]) + '\x1b[32m{}\x1b[m'
+			lastColumn = len(self.columns) -1
+			line = ''.join([c + e.get(flip, debug) + o.get(flip, debug, oddRange if lastColumn - i else 1) for i,(c,e,o) in enumerate(self.columns)]) + '\x1b[32m{}\x1b[m'
+			return '\n'.join([line.format(symbol, content) for symbol, content in e.getContent()])
+
 	def compose(self, node, verbose):
+		def _color(i): return '\x1b[{}m'.format(31 + i % 6)
 		tIndex = 0 # Column of target node
 		for i, c in enumerate(self.columns):
 			# If this is my column
-			if node.topName in c.name: yield '\x1b[m{}'.format(' '.join(node.getContent()[0]))
+			if node.topName in c.name: yield ('', EvenColumn.SOURCE, OddColumn.EMPTY) # '\x1b[m{}'.format(' '.join(node.getContent()[0]))
 			# Am I straight below the target?
 			elif node.topName in c.parents:
 				tIndex = i
 				c.parentSeen(node)
-				yield '\x1b[{}m{}←'.format(31 + tIndex % 6, '├' if c.parents else '└') # U+251c U+2514
-			else: yield '\x1b[{}m{}'.format(31 + tIndex % 6, '←←' if i else '│ ') # U+2502
+				yield (_color(tIndex), EvenColumn.RMERGE if c.parents else EvenColumn.LCORNER, OddColumn.LARROW)
+				# yield '\x1b[{}m{}←'.format(31 + tIndex % 6, '├' if c.parents else '└') # U+251c U+2514
+			# else: yield '\x1b[{}m{}'.format(31 + tIndex % 6, '←←' if i else '│ ') # U+2502
+			else: yield (_color(tIndex), EvenColumn.LARROW, OddColumn.LARROW) if i else (_color(tIndex), EvenColumn.PIPE, OddColumn.EMPTY)
 
 	def __init__(self):
 		self.columns = []
@@ -223,13 +292,17 @@ class DumbGrid:
 
 	def dealWith(self, node):
 		self.columns.append(self.Column(node))
-		self.rows.append((node.topName, ''.join([e for e in self.compose(node, False)])))
+		# self.rows.append((node.topName, ''.join([e for e in self.compose(node, False)])))
+		self.rows.append(self.Row(node.topName, [e for e in self.compose(node, False)]))
 
 	def done(self):
+		s = len(self.columns)
+		print('\tExtending rows to {} columns'.format(s))
+		for r in self.rows: r.extend(s)
 		return self.rows
 
-def unroll(grid, heads, db):
-	visit = Visit(heads)
+def unroll(grid, visit, heads, db):
+
 	while visit:
 		e = visit.pop()
 		grid.dealWith(e)
@@ -258,7 +331,8 @@ def deploy():
 			for s, t in e.getContent(): print(layout.format(s, t))
 			visit.push([db[p] for p in e.parents])
 
-		for e, row in unroll(DumbGrid(), heads, db): print(row.format(db[e]))
+		# for e, row in unroll(DumbGrid(), heads, db): print(row.format(db[e]))
+		for row in unroll(DumbGrid(), Visit(heads), heads, db): print(row.dump(db, 3))
 
 	except BrokenPipeError: pass
 
